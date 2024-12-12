c9d5022f857b50d749bca524b542885439f972fb
Olga Dombrovskaya <olgaekb26@gmail.com>
2024-12-12 19:26:24 +0000

    initial commit

---
+++ src/main/java/de/ait/Main.java
@@ -0,0 +1,11 @@
+package de.ait;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public static  String getGreeting (String name){
+        return " Hello Olga" + name + "!";
+    }
+}
